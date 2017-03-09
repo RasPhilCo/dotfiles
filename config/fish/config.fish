8015d066c487074bb2c1aa04f35ad9b345bd7449
RasPhilCo <capt.pn@gmail.com>
2017-03-08 16:50:55 -0800

    Add ls -fail abbreviation

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,3 +7,6 @@ alias ga 'git add'
 alias gcm 'git commit -m'
 alias gl "git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold
 green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+
+
+abbr ls 'ls -fail'
