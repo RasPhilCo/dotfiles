c518d443e787a3f9e2e60c60d09cb9a3be9cdc83
RasPhilCo <capt.pn@gmail.com>
2017-03-29 09:23:54 -0700

    Add gpf abbreviation

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,14 @@
-# git aliases
+# git
 alias g 'git'
-alias gst 'git status'
+alias gs 'git status'
 alias gd 'git diff'
 alias gco 'git checkout'
 alias ga 'git add'
 alias gcm 'git commit -m'
 alias gl "git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold
 green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+abbr gpf 'git pull; git fetch --prune'
 
-
+# darwin
 abbr ls 'ls -fail'
+
